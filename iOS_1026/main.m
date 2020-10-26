b254179aad98327a883f6d223924edd6352106a5
Chansik Yun <chansigi@gmail.com>
2020-10-26 11:57:03 +0900

    .

--- iOS_1026/main.m
+++ iOS_1026/main.m
@@ -8,12 +8,24 @@
 
 - (id)initWithName:(NSString*)name age:(int)age;
 
+// 중요: ObjC의 Getter / Setter의 이름 규칙은 매우 중요합니다.
 - (NSString*)name;
 - (int)age;
+
+- (void)setName:(NSString*)name;
+- (void)setAge:(int)age;
 
 @end
 
 @implementation User
+
+- (void)setName:(NSString*)name {
+  _name = name;
+}
+
+- (void)setAge:(int)age {
+  _age = age;
+}
 
 - (NSString*)name {
   return _name;
@@ -34,7 +46,20 @@
 
 @end
 
+// 1. ObjC에서 Getter / Setter의 이름 규칙을 제대로 사용한다면
+//    닷 연산자를 이용해서 Getter / Setter를 호출할 수 있습니다.
+
 int main() {
   User* user = [[User alloc] initWithName:@"Tom" age:42];
   NSLog(@"%@(%d)", [user name], [user age]);
+  
+  [user setName:@"Bob"];
+  [user setAge:16];
+  NSLog(@"%@(%d)", [user name], [user age]);
+  
+  user.name = @"Alice";  // [user setName:@"Alice"];
+  user.age = 24;         // [user setAge:24];
+  
+  NSLog(@"%@(%d)", user.name, user.age);
+  //             [user name], [user age]
 }
