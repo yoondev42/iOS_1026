1dea7bb615e9656eee5ecf5dc164f2a98cbe0b73
Chansik Yun <chansigi@gmail.com>
2020-10-26 09:49:40 +0900

    .

--- iOS_1026/main.m
+++ iOS_1026/main.m
@@ -1,42 +1,41 @@
-
-// 1. 확장자
-//      .m :  C언어 + Objective-C
-//     .mm :  C++  + Objective-C
-//      m: Message의 약자
-
-// 2. Xcode
-//           Build: Command + B
-//     Build + Run: Command + R
-
-// 3. 헤더파일
 #import <Foundation/Foundation.h>
-// ObjC의 핵심 기능을 모아놓은 헤더파일의 집합
 
-// #include: 중복 포함에 대해서 고려해야한다.
-//           => include guard
-// #import: include + include guard
-//          GNU Compiler의 확장 기능을 이용합니다.
-
-// 4. ObjC의 이름 규칙
-//     - NS 접두: NextStep(OS -> macOS)
-//      : namespace / package 같은 이름 충돌을 막는 문법이 제공되지 않습니다.
-
-int main() {
-  // C의 문자열
-  const char *s1 = "Hello, C";
-  printf("%s\n", s1);
-  
-  // ObjC의 문자열 클래스 - NSString
-  // - ObjC의 모든 객체는 반드시 힙에 생성되어야 합니다.
-  NSString *s2 = @"Hello, ObjC";
-  NSLog(@"%@", s2);
-}
+// 클래스 기본 문법
+// 1. ObjC는 smalltalk 언어의 영향을 받았습니다.
+//   => 다른 객체 지향 언어와는 용어적인 차이가 있습니다.
 
 
 #if 0
-#include <stdio.h>
+class Car {
+  
+};
+#endif
+
+// 클래스 선언부 - .h(헤더 파일)
+@interface Car : NSObject {
+  // 필드를 정의할 수 있습니다.
+@public
+  int speed;
+@protected
+  int color;
+@private
+  int n;
+}
+
+@end
+
+
+// 클래스 정의부 - .m(소스 파일)
+@implementation Car
+
+@end
 
 int main() {
-  printf("Hello, world..\n");
+  // Car* car = new Car;
+  
+  // Car의 클래스에 new라는 메세지를 전송하면, 새로운 객체를 생성할 수 있습니다.
+  Car* car = [Car new];
+  
+  car->speed = 100;
+  printf("%d\n", car->speed);
 }
-#endif
