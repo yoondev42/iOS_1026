6502fb5b52adc8eff4ea931e8cd4fb5f1bed7c99
Chansik Yun <chansigi@gmail.com>
2020-10-26 13:49:29 +0900

    .

--- iOS_1026/main.m
+++ iOS_1026/main.m
@@ -20,12 +20,37 @@
 @end
 
 @implementation Car
+
+// dealloc
+// ARC: 부모의 dealloc을 자동으로 호출한다.
+// MRC: 부모의 dealloc을 명시적으로 호출해야 합니다.
 - (void)dealloc {
   printf("Car 객체 파괴\n");
+  
+  [super dealloc];
 }
 @end
 
+
+// 1. 컴파일 옵션을 변경해서, ARC -> MRC 변경해야 합니다.
+// 2. MRC에서는 객체의 참조 계수를 증감을 개발자가 직접 관리해야 합니다.
+//   참조 계수 증가: retain
+//   참조 계수 감소: release
+
 int main() {
-  Car *p = [Car new];
+  // 1. 객체 생성시 최초의 참조 계수 값은 1 입니다.
+  Car *p1 = [Car new];
+  printf("Ref counting: %ld\n", [p1 retainCount]);
+  
+  // 2. 객체의 포인터 대입 후, 참조 계수는 증가해야 합니다.
+  Car *p2 = p1;
+  [p2 retain];
+  printf("Ref counting: %ld\n", [p1 retainCount]);
+  
+  // 3. 객체의 포인터를 더 이상 사용하지 않을 경우,
+  //    참조 계수를 감소시켜야 합니다.
+  [p1 release];
+  printf("Ref counting: %ld\n", [p1 retainCount]);  // 1
+  [p2 release];                                     // 1 -> 0 (객체 메모리 파괴)
   
 }
