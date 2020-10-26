4cfdb71491739c8ef56cebeb2199987ac0f2ac70
Chansik Yun <chansigi@gmail.com>
2020-10-26 15:01:44 +0900

    .

--- iOS_1026/main.m
+++ iOS_1026/main.m
@@ -10,6 +10,7 @@
 //       setPhone을 하는 순간, 참조 계수는 증가하지 않는다.
 
 // 소유권이 있을 경우
+#if 0
 @class Phone;
 @interface Person : NSObject {
   Phone* _phone;
@@ -80,8 +81,64 @@ int main() {
   [phone release];
   [person release];
 }
+#endif
 
+// Person 객체가 Phone객체를 소유하지 않을 경우 - 참조 계수 관리 X
+@class Phone;
+@interface Person : NSObject {
+  Phone* _phone;
+}
 
+- (id)initWithPhone:(Phone*)phone;
+
+- (void)setPhone:(Phone*)phone;
+- (void)dealloc;
+
+@end
+@implementation Person
+
+- (id)initWithPhone:(Phone *)phone {
+  self = [super init];
+  if (self) {
+    _phone = phone;
+  }
+  
+  return self;
+}
+
+- (void)setPhone:(Phone *)phone {
+    _phone = phone;
+}
+
+- (void)dealloc {
+  printf("Person 객체 파괴\n");
+  [super dealloc];
+}
+
+@end
+
+@interface Phone : NSObject
+- (void)dealloc;
+@end
+
+@implementation Phone
+- (void)dealloc {
+  printf("Phone 객체 파괴\n");
+  [super dealloc];
+}
+@end
+
+int main() {
+  Person* person = [[Person alloc] init];
+  Phone* phone = [[Phone alloc] init];
+  
+  [person setPhone:phone];
+  
+  [phone release];
+  printf("Person 객체 사용 중...\n");
+  // 이미 파괴된 Phone 객체를 참조할 위험성이 있습니다. - 댕글링 포인터
+  printf("Person 객체 사용 후...\n");
+}
 
 
 
