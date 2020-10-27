5324586ebaf50cd65e848c690fc96346528086e3
Chansik Yun <chansigi@gmail.com>
2020-10-27 16:17:55 +0900

    .

--- iOS_1026/main.m
+++ iOS_1026/main.m
@@ -2,11 +2,15 @@
 #import <Foundation/Foundation.h>
 
 // 1. 객체를 문자열로 출력할 때 - description
+// 2. Objective C에서 객체를 복사하는 방법 - Java: Cloneable / clone
+//   1) <NSCopying> Protocol 구현
+//   2) - (id)copyWithZone:(nullable NSZone *)zone 구현
+// 3. Setter에서 방어 복사본을 참조하는 방법
+//   @property(copy, nonatomic) Person* person;
 
-
-@interface Person : NSObject
+@interface Person : NSObject<NSCopying>
 @property(strong, nonatomic) NSString* name;
-@property(assign, nonatomic) int age;
+@property(assign, nonatomic, readonly) int age;
 
 - (id)initWithName:(NSString*)name age:(int)age;
 
@@ -31,10 +35,28 @@
   return self;
 }
 
+- (void)foo {
+  _age = 100;
+}
+
+// 일반 객체 생성 방법
+// 1. alloc
+// 2. initXXX
+
+// 복사본 생성 방법
+// 1. allocWithZone:    -  복사본 객체는 원본의 메모리 인접 영역에 생성한다.
+// 2. initXXX
+
+// 아래 함수에서 객체의 복사본을 생성하면 됩니다.
+- (id)copyWithZone:(nullable NSZone *)zone {
+  return [[Person allocWithZone:zone] initWithName:_name age:_age];
+}
+
 @end
 
 @interface Company : NSObject
-@property(strong, nonatomic) Person* person;
+// @property(strong, nonatomic) Person* person;
+@property(copy, nonatomic) Person* person;
 
 - (id)initWithPerson:(Person*)person;
 
@@ -51,7 +73,8 @@
 - (id)initWithPerson:(Person*)person {
   self = [super init];
   if (self) {
-    _person = person;
+    // _person = person;
+    _person = [person copy];  // 복사본을 참조한다.
   }
   return self;
 }
@@ -61,10 +84,23 @@
 int main() {
   Person* person = [[Person alloc] initWithName:@"Tom" age:42];
   Company* company = [[Company alloc] initWithPerson:person];
+
+  // Setter도 방어 복사본이 필요하다.
+  company.person = person;
+  
+  // company 객체가 알 수 없다.
+  person.name = @"Bob";
+  [person foo];
+  
+  // 해결방법: 단순히 person 객체를 불변 객체로 만드는 것도 중요하지만,
+  //         company가 person의 참조를 바로 사용하면 안된다.
+  //         => "방어 복사본"
+  
+  
+  
   
   NSLog(@"Person: %@", person);
   NSLog(@"Company: %@", company);
-  
 }
 
 
