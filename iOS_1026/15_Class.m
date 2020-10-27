691bc754ca8989831600d69ccf7cc278ffb186f9
Chansik Yun <chansigi@gmail.com>
2020-10-27 10:55:33 +0900

    .

---
+++ iOS_1026/15_Class.m
@@ -0,0 +1,115 @@
+
+#import <Foundation/Foundation.h>
+
+// Introspection: 내성, 자기성찰
+//   => "Reflection"과 동일한 개념
+//   : 실행 시간에 클래스의 정보 등을 탐색하는 목적으로 이용한다.
+
+@interface Car : NSObject
+- (void)go;
+@end
+
+@implementation Car
+- (void)go {
+  printf("Car go\n");
+}
+@end
+
+@interface Truck : Car
+- (void)go;
+- (void)foo;
+@end
+
+@implementation Truck
+// Overriding
+- (void)go {
+  printf("Truck go\n");
+}
+
+- (void)foo {
+  printf("Truck foo\n");
+}
+@end
+
+// 1. ObjC의 메소드는 '동적 바인딩'을 합니다.
+//  => '런타임에 어떤 객체 타입'을 참조하는지 조사해서, 함수를 호출하는 것
+//       Java, C++(virtual)
+
+// 2. Introspection 활용
+//    - Class: 클래스 타입의 타입
+//   1) 런타임에 어떤 객체 타입인지 알고 싶다.
+//   2) 런타임에 특정한 타입의 객체를 생성하고 싶다.
+//     => 프레임워크는 사용자의 클래스의 이름을 알 수 없다.
+//        사용자의 클래스의 객체를 생성하고 프레임워크에 등록하기 위해서는
+//        사용자 클래스의 이름 또는 클래스 타입 객체를 전달해야 한다.
+void goo(NSString* className) {
+  // 문자열로 전달된 클래스를 생성해서, go 라는 메소드를 호출하고 싶다.
+  Class c = NSClassFromString(className);
+  if (c) {
+    Car* p = [[c alloc] init];
+    [p go];
+  } else {
+    printf("해당 이름의 클래스 타입이 존재하지 않습니다.\n");
+  }
+}
+
+void foo(Car* p) {
+  // p가 Truck이면 foo를 호출하고 싶다.
+  if ([p class] == [Truck class]) {
+    Truck* t = (Truck*)p;
+    [t foo];
+  } else {
+    printf("p는 Truck이 아닙니다.\n");
+  }
+}
+
+
+int main() {
+  Car* p = [Truck new];
+  foo(p);
+  
+  goo(@"Car2");
+  
+  
+  // Class 얻는 방법 3가지
+  // 1) 타입
+  // Class c1 = [Car class];
+  // Class c2 = [Truck class];
+
+  // Car* car = [[Car alloc] init];
+  // Truck* truck = [[Truck alloc] init];
+  // 2) 객체
+  // Class c3 = [car class];
+  // Class c4 = [truck class];
+  
+  // 3) 이름(문자열) - 실패 가능성
+  // Class c5 = NSClassFromString(@"Car");
+  // NSLog(@"%@", c5);
+  
+}
+
+
+
+#if 0
+int main() {
+  Car* p1 = [[Car alloc] init];
+  [p1 go];
+  
+  Car* p2 = [[Truck alloc] init];
+  [p2 go];
+}
+#endif
+
+
+#if 0
+// Android - 동적 생성의 예제
+Intent intent = new Intent(this, MainActivity.class);
+startActivity(intent);
+#endif
+
+#if 0
+C       ->   Go
+C++     ->   Rust
+Java    ->   Kotlin
+ObjC    ->   Swift
+#endif
--- iOS_1026/main.m
+++ iOS_1026/main.m
@@ -1,110 +1,108 @@
 
 #import <Foundation/Foundation.h>
 
-// Introspection: 내성, 자기성찰
-//   => "Reflection"과 동일한 개념
-//   : 실행 시간에 클래스의 정보 등을 탐색하는 목적으로 이용한다.
+// 다형성(Polymorphism)
+//  1. 상속 - 부모 타입을 기반으로 다형성을 구현한다.
+//  2. 덕 타이핑(Duck typing)    - ObjC는 해당 다형성도 지원합니다.
+//    - 특정한 객체가 오리처럼 행동하면 오리로 봐야한다.
+
+@interface User : NSObject
+- (void)display;
+@end
+@implementation User
+- (void)display {
+  printf("User display\n");
+}
+@end
 
 @interface Car : NSObject
-- (void)go;
+- (void)display;
 @end
-
 @implementation Car
-- (void)go {
-  printf("Car go\n");
+- (void)display {
+  printf("Car display\n");
+}
+
+@end
+
+@interface Shape : NSObject
+- (void)display;
+@end
+@implementation Shape
+- (void)display {
+  printf("Shape display\n");
 }
 @end
 
-@interface Truck : Car
-- (void)go;
-- (void)foo;
-@end
-
-@implementation Truck
-// Overriding
-- (void)go {
-  printf("Truck go\n");
-}
-
-- (void)foo {
-  printf("Truck foo\n");
-}
-@end
-
-// 1. ObjC의 메소드는 '동적 바인딩'을 합니다.
-//  => '런타임에 어떤 객체 타입'을 참조하는지 조사해서, 함수를 호출하는 것
-//       Java, C++(virtual)
-
-// 2. Introspection 활용
-//    - Class: 클래스 타입의 타입
-//   1) 런타임에 어떤 객체 타입인지 알고 싶다.
-//   2) 런타임에 특정한 타입의 객체를 생성하고 싶다.
-//     => 프레임워크는 사용자의 클래스의 이름을 알 수 없다.
-//        사용자의 클래스의 객체를 생성하고 프레임워크에 등록하기 위해서는
-//        사용자 클래스의 이름 또는 클래스 타입 객체를 전달해야 한다.
-
-void goo(NSString* className) {
-  // 문자열로 전달된 클래스를 생성해서, go 라는 메소드를 호출하고 싶다.
-  Class c = NSClassFromString(className);
-  if (c) {
-    Car* p = [[c alloc] init];
-    [p go];
-  } else {
-    printf("해당 이름의 클래스 타입이 존재하지 않습니다.\n");
-  }
-}
-
-void foo(Car* p) {
-  // p가 Truck이면 foo를 호출하고 싶다.
-  if ([p class] == [Truck class]) {
-    Truck* t = (Truck*)p;
-    [t foo];
-  } else {
-    printf("p는 Truck이 아닙니다.\n");
+int main() {
+  User* user = [User new];
+  Car* car = [Car new];
+  Shape* shape = [Shape new];
+  
+  NSArray* arr = [NSArray arrayWithObjects:user, car, shape, nil];
+  for (int i = 0 ; i < arr.count; ++i) {
+    id p = [arr objectAtIndex:i];
+    [p display];
+    // 해당 객체가 수신된 메소드를 호출할 수 없을 경우, '런타임 오류가 발생'합니다.
   }
 }
 
 
-int main() {
-  Car* p = [Truck new];
-  foo(p);
-  
-  goo(@"Car2");
-  
-  
-  // Class 얻는 방법 3가지
-  // 1) 타입
-  // Class c1 = [Car class];
-  // Class c2 = [Truck class];
-
-  // Car* car = [[Car alloc] init];
-  // Truck* truck = [[Truck alloc] init];
-  // 2) 객체
-  // Class c3 = [car class];
-  // Class c4 = [truck class];
-  
-  // 3) 이름(문자열) - 실패 가능성
-  // Class c5 = NSClassFromString(@"Car");
-  // NSLog(@"%@", c5);
-  
-}
 
 
 
+
+
+
+
+
+//  1. 상속 - 부모 타입을 기반으로 다형성을 구현한다.
 #if 0
+@interface Shape : NSObject
+- (void)draw;
+@end
+@implementation Shape
+- (void)draw {
+  printf("Shape draw\n");
+}
+@end
+
+@interface XRect : Shape
+- (void)draw;
+@end
+@implementation XRect
+- (void)draw {
+  printf("Rect draw\n");
+}
+@end
+
+@interface XCircle : Shape
+- (void)draw;
+@end
+@implementation XCircle
+- (void)draw {
+  printf("Circle draw\n");
+}
+@end
+
+@interface XTriangle : Shape
+- (void)draw;
+@end
+@implementation XTriangle
+- (void)draw {
+  printf("Triangle draw\n");
+}
+@end
+
 int main() {
-  Car* p1 = [[Car alloc] init];
-  [p1 go];
+  XRect* rect = [[XRect alloc] init];
+  XCircle* circle = [[XCircle alloc] init];
+  XTriangle* triangle = [[XTriangle alloc] init];
+  NSArray* arr = [NSArray arrayWithObjects:rect, circle, triangle, nil];
   
-  Car* p2 = [[Truck alloc] init];
-  [p2 go];
+  for (int i = 0 ; i < arr.count; ++i) {
+    Shape* s = [arr objectAtIndex:i];
+    [s draw];
+  }
 }
 #endif
-
-
-#if 0
-// Android - 동적 생성의 예제
-Intent intent = new Intent(this, MainActivity.class);
-startActivity(intent);
-#endif
-
