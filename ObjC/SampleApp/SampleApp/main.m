02711d5ed48686dcbc831b5ad5db83acdff35b1b
Chansik Yun <chansigi@gmail.com>
2020-10-28 17:23:46 +0900

    .

--- ObjC/SampleApp/SampleApp/main.m
+++ ObjC/SampleApp/SampleApp/main.m
@@ -1,18 +1,7 @@
-//
-//  main.m
-//  SampleApp
-//
-//  Created by Chansik Yun on 2020/10/28.
-//
 
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
 
-int main(int argc, char * argv[]) {
-  NSString * appDelegateClassName;
-  @autoreleasepool {
-      // Setup code that might create autoreleased objects goes here.
-      appDelegateClassName = NSStringFromClass([AppDelegate class]);
-  }
-  return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+#import <Foundation/Foundation.h>
+
+int main() {
+  printf("Hello, iOS\n");
 }
