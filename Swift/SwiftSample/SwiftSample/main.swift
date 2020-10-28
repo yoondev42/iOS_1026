5174016401743a6b8f84cdcf3a7a3770a5656c5d
Chansik Yun <chansigi@gmail.com>
2020-10-28 14:02:25 +0900

    .

--- Swift/SwiftSample/SwiftSample/main.swift
+++ Swift/SwiftSample/SwiftSample/main.swift
@@ -32,6 +32,7 @@ class User : CustomStringConvertible {
 }
 #endif
 
+#if false
 struct User {
   let name: String
   let age: Int
@@ -44,4 +45,95 @@ struct User {
 let user = User(name: "Tom", age: 42)
 user.move()
 print(user)
+#endif
+
+// 3. 구조체는 let으로 사용하면, 불변 객체가 된다.
+//    클래스는 let으로 사용해도, 불변 객체가 아니다.
+#if false
+class UserClass {
+  var age: Int = 0
+}
+
+struct UserStruct {
+  var age: Int = 0
+}
+
+// final User p1 = UserClass()
+let p1 = UserClass()  // Reference Type
+p1.age = 42
+
+let p2 = UserStruct() // Value Type
+// p2.age = 42
+#endif
+
+// 4. 클래스는 참조 계수 기반으로 관리되지만
+//    구조체는 참조 계수 기반으로 관리되지 않는다.
+
+// 5. 클래스 vs 구조체
+//   1) 참조 계수를 증감하는 동작은 멀티 스레드에서 안전하게 동작해야 한다.(원자적 연산)
+//      깊은 복사를 하는 것이 성능이 더 좋을 수 있다.
+//   2) 힙에 객체를 생성하는 연산과 힙의 객체를 반환하는 연산은 오버헤드가 있다.
+//      스택은 객체를 생성하고 파괴되는 비용이 없다.
+
+#if false
+struct User {
+
+}
+
+func foo() {
+  var user: User? = User()   // 1
+  var user2: User? = user    // 2
+  // class: 참조 복사(얕은 복사) - 참조 계수 관리
+  // struct: 깊은 복사        - 참조 계수 관리 X
+  
+  user = nil                 // 1
+  user2 = nil                // 0
+}
+
+foo()
+#endif
+
+// 6. 구조체의 메소드는 self를 불변으로 취급한다.
+//   mutating: 메소드에서 프로퍼티의 값을 변경할 수 있다.
+//   불변 객체(let)에서는 mutating 메소드를 호출하는 것이 불가능하다.
+struct Point {
+  var x: Int
+  var y: Int
+  
+  mutating func move(dx: Int, dy: Int) {
+    x += dx
+    y += dy
+  }
+  
+  func printPoint() {
+    print(self)
+  }
+}
+
+let point = Point(x: 10, y: 20)
+
+// point.move(dx: 2, dy: 3)
+point.printPoint()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
