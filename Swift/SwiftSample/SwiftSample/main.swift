4d10689d7a74ea4786a77fcbd9fbdabfced874b9
Chansik Yun <chansigi@gmail.com>
2020-10-28 11:42:49 +0900

    .

--- Swift/SwiftSample/SwiftSample/main.swift
+++ Swift/SwiftSample/SwiftSample/main.swift
@@ -23,6 +23,7 @@ image = nil
 print("image = nil")
 #endif
 
+#if false
 class Node {
   // Strong reference
   // var next: Node?
@@ -55,6 +56,29 @@ n2.next = n1
 print(n2.next) // ?
 n1 = nil
 print(n2.next) // ?
+#endif
+
+// 생성 시간이 오래 걸린다.
+//   => Lazy Initialization(지연 초기화)
+//      - 처음 접근되는 시점에 생성하는 것이 좋다.
+class Image {
+  init() {
+    print("Image 객체 생성")
+  }
+}
+
+class Person {
+  lazy var image: Image = Image()
+  
+  init() {
+    print("Person 객체 생성")
+  }
+}
+
+let person = Person()
+print("Person 객체 생성 후")
+print(person.image)
+
 
 
 
