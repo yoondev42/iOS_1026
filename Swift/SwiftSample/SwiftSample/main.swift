e277c70b231834976bcc81a0a08feb47bc269730
Chansik Yun <chansigi@gmail.com>
2020-10-28 15:44:36 +0900

    .

--- Swift/SwiftSample/SwiftSample/main.swift
+++ Swift/SwiftSample/SwiftSample/main.swift
@@ -39,6 +39,7 @@ let school = School.primary
 printSchool(school: school)
 #endif
 
+#if false
 // 2. enum은 원시값(rawValue)을 지원합니다.
 //  - Int, String, Double등의 값을 가질 수 있습니다.
 enum School : String {
@@ -54,6 +55,7 @@ enum School : String {
 
 // 3. Swift enum의 가장 강력한 기능
 //    연관값(Associated Value)
+
 
 enum NetworkError {
   case invalidParameter(String)
@@ -106,4 +108,70 @@ func printError(_ error: NetworkError) {
 printError(error1)
 printError(error2)
 printError(error3)
+#endif
+
+// Optional
+//enum XOptional<T> {
+//  case none
+//  case some(T)
+//}
+//
+// var p: XOptional<Int> = .some(42)
+// var p: Optional<Int> = .some(42)
+#if false
+var p: Int? = 42     // .some(42)
+
+switch p {
+case .none:
+  print("값이 존재하지 않습니다.")
+case let .some(value):
+  print("value=\(value)")
+}
+
+if let p = p {
+  print(p)
+} else {
+  print("값이 존재하지 않습니다.")
+}
+#endif
+
+class User {
+  var name: String = "Tom"
+  
+  func hello() {
+    print("User hello")
+  }
+}
+
+
+// 1. User   - nil 가능성이 없는 타입
+// 2. User?  - nil 가능성이 있기 떄문에
+//    : Optional Binding / Optional Chaining 을 통해 안전하게 사용해야 한다.
+//
+// 3. User!: 암시적 추출 타입
+// => nil 가능성이 있는 타입이지만, nil 체크를 하지 않아도 된다.
+//    nil일 경우 예외로 인해 프로그램이 비정상 종료 한다.
+//  : 사용하지 않는 것이 좋다.
+
+var user: User! = nil
+user.hello()
+
+#if false
+var user: User? = User()
+// Optional Binding
+if let user = user {
+  user.hello()
+}
+
+// Optional Chaining
+user?.hello()
+
+let name: String? = user?.name
+if let name = user?.name {
+  print(name)
+}
+
+#endif
+
+
 
