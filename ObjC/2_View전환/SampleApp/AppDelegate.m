2ecb3b0517f3ac1392327a409f47bea96cef5989
Chansik Yun <chansigi@gmail.com>
2020-10-29 14:02:43 +0900

    .

---
+++ ObjC/2_View전환/SampleApp/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  SampleApp
+//
+//  Created by Chansik Yun on 2020/10/29.
+//
+
+#import "AppDelegate.h"
+#import "ViewController.h"
+
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  
+  self.window = [[UIWindow alloc] initWithFrame: [[UIScreen mainScreen] bounds]];
+  
+  _window.rootViewController = [[ViewController alloc] init];
+  
+  [_window makeKeyAndVisible];
+  
+  return YES;
+}
+
+@end
---
+++ ObjC/2_View전환/SampleApp/ViewController.m
@@ -0,0 +1,27 @@
+#import "ViewController.h"
+
+
+// 화면 전환 하는 방법
+//  - Android
+//    => Activity 전환
+//       Fragment 전환
+
+//  - iOS
+//    => 화면을 전환하는 다양한 방법
+
+// UIWindow
+//    - rootViewController: ViewController
+//                              - UIView* view
+//     1) Interface Builder
+
+//     2) Storyboard
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view.
+}
+
+
+@end
