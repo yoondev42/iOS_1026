b5495cc6ab7f78b2f71178cbc47a420aa2bff480
Chansik Yun <chansigi@gmail.com>
2020-10-30 10:23:41 +0900

    .

--- Swift/5_TableView/SampleApp/MyViewController.swift
+++ Swift/5_TableView/SampleApp/MyViewController.swift
@@ -29,7 +29,6 @@ extension MyViewController: UITableViewDataSource {
   
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-   
     let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
     cell.textLabel?.text = "Cell - \(indexPath)"
     return cell
@@ -53,9 +52,6 @@ extension MyViewController: UITableViewDataSource {
   #endif
   
   
-  
-  
-  
   // Cell을 데이터의 개수 만큼 생성하는 것이 아니라,
   // 화면에 필요한만큼만 생성하고, 나머지의 데이터는 기존의 Cell을 재활용해야 한다.
   // => View Holder Pattern
@@ -67,3 +63,19 @@ extension MyViewController: UITableViewDataSource {
   }
   #endif
 }
+
+// iOS 리소스 관리
+
+// - 레티나 디스플레이
+//   : 프로그램을 작성할 때는 375 x 667을 기준으로 작성하지만,
+//     이미지 리소스를 표현할 때는 배율에 따라서 더 큰 크기의 이미지를 제공해야 한다.
+
+// 50 x 50
+//    2x - 100 x 100
+//    3x - 150 x 150
+
+// - iPhone SE 375 x 667
+//             750 x 1334
+
+// - iPhone 12 Pro Max    926 x 428
+//                        2778 x 1284
