e07168f7acbcdde7ba133ad9a3bf81c9e31ca122
Chansik Yun <chansigi@gmail.com>
2020-10-30 09:58:04 +0900

    .

--- Swift/5_TableView/SampleApp/MyViewController.swift
+++ Swift/5_TableView/SampleApp/MyViewController.swift
@@ -1,29 +1,56 @@
-//
-//  MyViewController.swift
-//  SampleApp
-//
-//  Created by Chansik Yun on 2020/10/30.
-//
-
 import UIKit
 
 class MyViewController: UIViewController {
+  @IBOutlet var tableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    tableView.dataSource = self
+  }
+}
 
-        // Do any additional setup after loading the view.
+// extension을 이용하면, 기존 클래스에 새로운 메소드를 추가하는 것이 가능합니다.
+// 새로운 프로토콜을 추가적으로 구현하는 것도 가능합니다.
+
+extension MyViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 2
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 20
+  }
+  
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+   
+    var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: "MyCell")
+    if (cell == nil) {
+      // cell = UITableViewCell() // !
+      cell = UITableViewCell(style: .default, reuseIdentifier: "MyCell")
+      print("Cell 생성 - \(indexPath)")
+    } else {
+      print("Cell 재활용 - \(indexPath)")
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
+    cell?.textLabel?.text = "Cell - \(indexPath)"
+    return cell!
+  }
+  
+  
+  
+  
+  
+  // Cell을 데이터의 개수 만큼 생성하는 것이 아니라,
+  // 화면에 필요한만큼만 생성하고, 나머지의 데이터는 기존의 Cell을 재활용해야 한다.
+  // => View Holder Pattern
+  #if false
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    cell.textLabel?.text = "Cell"
+    return cell
+  }
+  #endif
 }
