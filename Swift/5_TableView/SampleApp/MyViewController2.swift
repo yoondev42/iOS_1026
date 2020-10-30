35ff913f71ddef0449e573bbc9c628d250de5ba5
Chansik Yun <chansigi@gmail.com>
2020-10-30 10:01:57 +0900

    .

---
+++ Swift/5_TableView/SampleApp/MyViewController2.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+
+class MyViewController2: UIViewController {
+  
+  @IBOutlet var tableView: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+}
