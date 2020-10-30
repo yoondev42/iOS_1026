37b23bdb1d363c15d6619167c4bfbd5d858024c9
Chansik Yun <chansigi@gmail.com>
2020-10-30 12:01:42 +0900

    .

--- Swift/6_NavigationController/SampleApp/FirstController.swift
+++ Swift/6_NavigationController/SampleApp/FirstController.swift
@@ -1,6 +1,16 @@
 
 import UIKit
 
+
+//   - UIViewController
+//       => present / dismiss
+//   - UINavigationController
+//       => pushViewController / popViewController
+//         - FirstController
+//         - SecondController
+//         - ThirdController
+
+
 class FirstController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -10,6 +20,16 @@ class FirstController: UIViewController {
 
   @IBAction func onTouch(_ sender: Any) {
     // performSegue(withIdentifier: "ShowSecondSegue", sender: self)
+    
+    // 1. 이동하고자 하는 컨트롤러 생성(XIB 동일)
+    let controller = SecondController.create()
+    
+    // present(controller, animated: true, completion: nil)
+    
+    // NavigationController안의 모든 뷰 컨트롤러는 아래의 속성을 통해 자신의 NavigationController에 접근 가능합니다.
+    navigationController?.pushViewController(controller, animated: true)
+    
+    
   }
 
   static func create() -> FirstController {
@@ -17,6 +37,8 @@ class FirstController: UIViewController {
     return storyboard.instantiateViewController(withIdentifier: "FirstController") as! FirstController
   }
 }
+
+
 
 class SecondController: UIViewController {
   override func viewDidLoad() {
@@ -27,6 +49,9 @@ class SecondController: UIViewController {
 
   @IBAction func onTouch(_ sender: Any) {
     // performSegue(withIdentifier: "ShowThirdSegue", sender: self)
+    
+    let controller = ThirdController.create()
+    navigationController?.pushViewController(controller, animated: true)
   }
 
   @IBAction func unwind(segue: UIStoryboardSegue) {}
@@ -44,10 +69,16 @@ class ThirdController: UIViewController {
     // Do any additional setup after loading the view.
   }
 
-  @IBAction func onTouch(_ sender: Any) {}
+  @IBAction func onTouch(_ sender: Any) {
+    // navigationController?.popViewController(animated: true)  - 이전
+    // navigationController?.popToRootViewController(animated: true) - 처음
+    // navigationController?.popToViewController(controller, animated: true) - 특정 컨트롤러
+    
+    dismiss(animated: true, completion: nil)
+  }
 
   static func create() -> ThirdController {
     let storyboard = UIStoryboard(name: "Main", bundle: nil)
-    return storyboard.instantiateViewController(withIdentifier: "SecondController") as! ThirdController
+    return storyboard.instantiateViewController(withIdentifier: "ThirdController") as! ThirdController
   }
 }
