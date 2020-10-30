1ae3b4df31e672b9ef6daf08356d3fbf70eb1d6b
Chansik Yun <chansigi@gmail.com>
2020-10-30 15:48:30 +0900

    .

---
+++ Swift/8_NSURL/SampleApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Chansik Yun on 2020/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+  @IBAction func loadImage1(_ sender: UIButton) {
+  }
+  @IBAction func loadImage2(_ sender: UIButton) {
+  }
+  @IBAction func loadImage3(_ sender: UIButton) {
+  }
+  @IBAction func loadImage4(_ sender: UIButton) {
+  }
+  @IBAction func loadImage5(_ sender: UIButton) {
+  }
+  
+}
+
