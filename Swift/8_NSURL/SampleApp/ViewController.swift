40cb52baa40e1ee8716024f436bb723950df7b86
Chansik Yun <chansigi@gmail.com>
2020-10-30 15:51:16 +0900

    .

--- Swift/8_NSURL/SampleApp/ViewController.swift
+++ Swift/8_NSURL/SampleApp/ViewController.swift
@@ -1,29 +1,36 @@
-//
-//  ViewController.swift
-//  SampleApp
-//
-//  Created by Chansik Yun on 2020/10/29.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet var imageView: UIImageView!
+  
+  static let imageURL = "https://media.playstation.com/is/image/SCEA/cyberpunk-2077-listing-thumb-01-ps4-06jun19-ko-kr?$native_nt$"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
-
+  
   @IBAction func loadImage1(_ sender: UIButton) {
-  }
-  @IBAction func loadImage2(_ sender: UIButton) {
-  }
-  @IBAction func loadImage3(_ sender: UIButton) {
-  }
-  @IBAction func loadImage4(_ sender: UIButton) {
-  }
-  @IBAction func loadImage5(_ sender: UIButton) {
+    
   }
   
+  @IBAction func loadImage2(_ sender: UIButton) {
+    
+  }
+  
+  @IBAction func loadImage3(_ sender: UIButton) {
+    
+  }
+  
+  @IBAction func loadImage4(_ sender: UIButton) {
+    
+  }
+  
+  @IBAction func loadImage5(_ sender: UIButton) {
+    
+  }
 }
 
